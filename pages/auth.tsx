5a645e74aaddd83060fb36eeb2b5dea3ff774950
anil <anilshrestha0403.as@gmail.com>
2023-05-14 10:20:10 -0700

    Google and GitHub authentication created

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import axios from "axios";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 const Auth = () => {
   //router
@@ -95,6 +97,40 @@ const Auth = () => {
             >
               {variant === "login" ? "Login" : "Register"}
             </button>
+            <div className="flex flex-row items-center justify-center gap-4 mt-8">
+              <div
+              onClick={() => signIn("google", { callbackUrl: "/" })}
+                className="
+                w-10
+                h-10
+                bg-white
+                rounded-full
+                flex
+                items-center
+                justify-center
+                cursor-pointer
+                hover:opacity-80
+                transition"
+              >
+                <FcGoogle size={30} />
+              </div>
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/" })}
+                className="
+                w-10
+                h-10
+                bg-white
+                rounded-full
+                flex
+                items-center
+                justify-center
+                cursor-pointer
+                hover:opacity-80
+                transition"
+              >
+                <FaGithub size={30} />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-8 text-center">
               {variant === "login"
                 ? "First time using Netflix?"
